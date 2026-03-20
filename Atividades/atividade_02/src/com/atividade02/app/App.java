03a750d0359f4b482e672197ba11a545d4bb3419
julianacmsantos <julianacsmartinez@gmail.com>
2026-03-19 21:47:38 -0300

    Aula 19/03/26 + Atividade 02

---
+++ Atividades/atividade_02/src/com/atividade02/app/App.java
@@ -0,0 +1,98 @@
+package com.atividade02.app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Crie um programa que receba do usuário o nome e a idade (1x)
+        // e depois exiba uma lista de filmes
+        // Sala 01 - A Roda Quadrada - Livre
+        // Sala 02 - A Volta dos que não foram - 12 anos
+        // Sala 03 - Poeira em Alto Mar - 14 anos
+        // Sala 04 - As Tranças do Rei Careca - 16 anos
+        // Sala 05 - A vingança do Peixe Frito - 18 anos
+        // O usuário deverá escolher o filme desejado
+        // se tiver a idade mínima para ver o filme, o programa imprime o ingresso e encerra
+        // se não tiver a idade mínima, o programa bloqueia a entrada e re-exibe a lista de filmes 
+        // para que o usuário possa escolher outro filme
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        int idade;
+        int codFilme;
+        String filmes;
+        int ingressoEmitido = 0;
+
+        filmes = "1 - A Roda Quadrada \n2 - A Volta dos que não foram \n3 - Poeira em Alto Mar \n4 - As Tranças do Rei Careca \n5 - A vingança do Peixe Frito";
+
+        System.out.println("Informe seu nome:");
+        nome = sc.nextLine();
+
+        System.out.println("Informe sua idade:");
+        idade = sc.nextInt();
+
+        System.err.println("Bem-vindo(a) ao cinema, " + nome + "!");
+
+        do {
+        System.err.println("\n--- FILMES EM CARTAZ ---\n");
+        System.err.println(filmes);
+        System.out.println("\nEscolha o filme que deseja ver:");
+        codFilme = sc.nextInt();
+
+        switch (codFilme) {
+            case (1):
+                System.out.println("\nBom filme, "  + nome + "!");
+                break; 
+            case (2):
+                if (idade >= 12) {
+                    System.out.println("\nBom filme, " + nome + "!");
+                }
+                else {
+                    System.out.println("\nInfelizmente você não pode assistir esse filme, "  + nome + "!");
+                    System.out.println("Classificação indicativa: 12 anos");
+                    System.out.println("\nEscolha outro filme:");                    
+                    ingressoEmitido = 1;
+                }
+                break;
+            case (3):
+                if (idade >= 14) {
+                    System.out.println("\nBom filme, " + nome + "!");
+                }
+                else {
+                    System.out.println("\nInfelizmente você não pode assistir esse filme, "  + nome + "!");
+                    System.out.println("Classificação indicativa: 14 anos");
+                    System.out.println("\nEscolha outro filme:");                    
+                    ingressoEmitido = 1;
+                }
+                break;
+            case (4):
+                if (idade >= 16) {
+                    System.out.println("\nBom filme, " + nome + "!");
+                }
+                else {
+                    System.out.println("\nInfelizmente você não pode assistir esse filme, " + nome + "!");
+                    System.out.println("Classificação indicativa: 16 anos");
+                    System.out.println("\nEscolha outro filme:");                    
+                    ingressoEmitido = 1;
+                }
+                break;
+            case (5):
+                if (idade >= 18) {
+                    System.out.println("\nBom filme, " + nome + "!");
+                }
+                else {
+                    System.out.println("\nInfelizmente você não pode assistir esse filme, "  + nome + "!");
+                    System.out.println("Classificação indicativa: 18 anos");
+                    System.out.println("\nEscolha outro filme:");                    
+                    ingressoEmitido = 1;
+                }
+                break;
+        }
+    } while (ingressoEmitido == 1);
+
+    
+        sc.close();
+    }
+}
---
+++ Parte01-Logica_de_programacao/11_do_while/src/com/catraca/app/App.java
@@ -0,0 +1,48 @@
+package com.catraca.app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        double peso, altura;
+        int opcao;
+
+        do {
+            System.out.println("Deseja registrar nova entrada?");
+            System.out.println("1 - Registrar nova entrada");
+            System.out.println("2 - Sair do programa");
+            opcao = sc.nextInt();
+
+            if (opcao == 1) {
+                sc.nextLine();
+                System.out.println("Informe seu nome:");
+                nome = sc.nextLine();
+
+                System.out.println("Informe seu peso em kg:");
+                peso = sc.nextDouble();
+
+                System.out.println("Informe sua altura em metros:");
+                altura = sc.nextDouble();
+
+                if (peso < 120 && altura >= 1.25) {
+                    System.out.println("Entrada de " + nome + " autorizada.");
+                }
+                else {
+                    System.out.println("Entrada de " + nome + " não autorizada.");
+                }
+            }
+            else if (opcao == 2) {
+                System.out.println("Obrigado pela preferência!");
+            }            
+            else {
+                System.out.println("Opção inválida");
+            }
+        } while (opcao != 2);
+
+        sc.close();
+    }
+}
